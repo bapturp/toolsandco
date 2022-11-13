093dbb6fb8927eb75d9ed0339dc37fda51e99a86
Hugo Violas <violas.hugo@gmail.com>
2022-11-13 12:48:32 +0100

    Created Reservation model, routes and views with form, started working on date substraction to get the number of reservation days

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 /* GET home page */
 router.use("/tools", require("./tools.routes"));
+router.use("/reservation", require("./reservation.routes"));
 router.get("/", (req, res, next) => {
   res.render("index");
 });
