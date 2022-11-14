b2ef59e78408e05cf179e40b7b2d3a65b0d8f99b
Ines Zallouz <ines.zallouz@gmail.com>
2022-11-14 16:44:51 +0100

    Try to implement ajax search

--- routes/search.routes.js
+++ routes/search.routes.js
@@ -4,18 +4,8 @@ const Tool = require("./../models/Tool.model");
 const Tooltype = require("./../models/Tooltype.model");
 const Usecase = require("./../models/Usecase.model");
 
+// Display all tools if no query, else display corresponding search results
 router.get("/", async (req, res, next) => {
-    try {
-        const toolsList = await Tool.find().populate(['tool_type', 'use_case']);
-        const tooltypesList = await Tooltype.find()
-        const usecasesList = await Usecase.find()
-        res.render("search", { toolsList, tooltypesList, usecasesList })
-    } catch (error) {
-        next(error)
-    }
-})
-
-router.get("/search-tool", async (req, res, next) => {
     try {
         const queriedTools = {}
         const query = req.query
@@ -31,10 +21,11 @@ router.get("/search-tool", async (req, res, next) => {
             const startDate = query.start_date
             const endDate = query.end_date
         }
+        const toolsList = await Tool.find({ $and: [queriedTools] }).populate(['tool_type', 'use_case']);
+        const tooltypesList = await Tooltype.find()
+        const usecasesList = await Usecase.find()
 
-
-        const toolsList = await Tool.find({ $and: [queriedTools] })
-        res.render("search", { toolsList })
+        res.render("search", { toolsList, tooltypesList, usecasesList })
     } catch (error) {
         next(error)
     }
