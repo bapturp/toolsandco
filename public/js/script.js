83fefbab1d43e37674c8f6331ffab1cad9e79e75
Hugo Violas <violas.hugo@gmail.com>
2022-11-14 16:32:43 +0100

    Added basic cart function

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,17 @@
+const addToCartBtns = document.querySelectorAll(".add-to-cart");
+
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
 document.addEventListener("DOMContentLoaded", () => {
   console.log("app JS imported successfully!");
+});
+
+addToCartBtns.forEach((addToCartBtn) => {
+  addToCartBtn.addEventListener("click", async (event) => {
+    event.preventDefault();
+    console.log(addToCartBtn.innerHTML);
+    const route = event.target.closest("form").action;
+    console.log(route);
+    const { data } = await axios.get(route);
+    console.log("=======", data);
+  });
 });
--- routes/tools.routes.js
+++ routes/tools.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Tool = require("./../models/Tool.model");
+// const cartArray = [];
 
 router.get("/", async (req, res, next) => {
   const toolsList = await Tool.find();
@@ -13,5 +14,21 @@ router.get("/:id", async (req, res, next) => {
   console.log(tool);
   res.render("tool", { tool });
 });
+
+router.get("/:id/addToCart", async (req, res, next) => {
+  const foundTool = await Tool.findById(req.params.id);
+  console.log("========");
+  const id = req.params.id;
+  req.session.cart.push(id);
+  // addToCart(id);
+  console.log(req.session.cart);
+  console.log(foundTool);
+  res.status(200).json(req.session.cart);
+  //return foundTool;
+});
+
+function addToCart(id) {
+  cartArray.push(id);
+}
 
 module.exports = router;
