c8d7b3a83aae86a1b42140751f52b0975cf9ebc3
Baptiste Dromer <baptiste.dromer@gmail.com>
2022-11-14 10:00:32 +0100

    Renamed bin to seeds

---
+++ seeds/tools.seed.js
@@ -0,0 +1,10 @@
+require('../db/index');
+const Tool = require('../models/Tool.model');
+const tools = require('./tools.json');
+
+async function toolSeed() {
+  await Tool.deleteMany();
+  await Tool.create(tools);
+}
+
+toolSeed();
