9381faad49581140243bc5325833dc74b1d24050
Baptiste Dromer <baptiste.dromer@gmail.com>
2022-11-14 12:43:51 +0100

    extends

--- seeds/tools.seed.js
+++ seeds/tools.seed.js
@@ -1,10 +1,12 @@
 require('../db/index');
 const Tool = require('../models/Tool.model');
 const tools = require('./tools.json');
+const mongoose = require('mongoose');
 
 async function toolSeed() {
   await Tool.deleteMany();
   await Tool.create(tools);
+  mongoose.disconnect();
 }
 
 toolSeed();
