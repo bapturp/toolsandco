4fb379de2e7a6288b2a381ccf8d5f2f21e7fde50
Ines Zallouz <ines.zallouz@gmail.com>
2022-11-14 12:43:09 +0100

    Add seeders

--- seeds/tools.seed.js
+++ seeds/tools.seed.js
@@ -1,10 +1,37 @@
 require('../db/index');
 const Tool = require('../models/Tool.model');
 const tools = require('./tools.json');
+const Tooltype = require('../models/Tooltype.model');
+const tooltypes = require('./tooltypes.json');
+const Usecase = require('../models/Usecase.model');
+const usecases = require('./usecases.json');
 
 async function toolSeed() {
+  await tooltypeSeed()
+  await usecaseSeed()
+
   await Tool.deleteMany();
-  await Tool.create(tools);
+  for (const tool of tools) {
+    const type = await Tooltype.findOne({ name: tool.tool_type })
+    tool.tool_type = type._id
+    const usecases = await Usecase.find({ name: { $in: tool.use_case } })
+    const usecasesId = usecases.map((usecase) => {
+      return usecase._id
+    })
+    tool.use_case = usecasesId
+    await Tool.create(tool)
+
+  }
+}
+
+async function tooltypeSeed() {
+  await Tooltype.deleteMany();
+  await Tooltype.create(tooltypes);
+}
+
+async function usecaseSeed() {
+  await Usecase.deleteMany();
+  await Usecase.create(usecases);
 }
 
 toolSeed();
