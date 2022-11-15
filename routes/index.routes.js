124f3a79171ea8b67a42e370e8fcefa632afdafd
Hugo Violas <violas.hugo@gmail.com>
2022-11-15 14:30:11 +0100

    Resolved mergin bugs

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const exposeUserInfo = require("../middleware/exposeUserInfo");
+const exposeUserInfo = require("../middlewares/exposeUserInfo");
 const router = express.Router();
 const Tooltype = require("./../models/Tooltype.model");
 const Usecase = require("./../models/Usecase.model");
@@ -12,8 +12,8 @@ router.use("/search", require("./search.routes"));
 router.use("/cart", require("./cart.routes"));
 
 router.get("/", exposeUserInfo, async (req, res, next) => {
-  const tooltypesList = await Tooltype.find()
-  const usecasesList = await Usecase.find()
+  const tooltypesList = await Tooltype.find();
+  const usecasesList = await Usecase.find();
   res.render("index", { isHome: true, tooltypesList, usecasesList });
 });
 
