cf81c46ce96b458721ebc07f2865f4234306d2c0
Baptiste Dromer <baptiste.dromer@gmail.com>
2022-11-15 11:17:57 +0100

    added expose user info middleware

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const exposeUserInfo = require("../middleware/exposeUserInfo");
 const router = express.Router();
 
 /* GET home page */
 router.use("/tools", require("./tools.routes"));
 router.use("/reservation", require("./reservation.routes"));
 router.use("/search", require("./search.routes"));
-router.get("/", (req, res, next) => {
+router.get("/", exposeUserInfo, (req, res, next) => {
   res.render("index");
 });
 
