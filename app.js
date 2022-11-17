bf582aeb4f9a784d0784a4e61651c65679ecc16b
Baptiste Dromer <baptiste.dromer@gmail.com>
2022-11-17 12:22:18 +0100

    implement redirect to checkout

--- app.js
+++ app.js
@@ -1,59 +1,54 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv").config();
+require('dotenv').config();
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express');
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
-hbs.registerPartials(__dirname + "/views/partials");
+const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
-
-const capitalize = require("./utils/capitalize");
-const projectName = "app";
-
-app.locals.appTitle = `${capitalize(projectName)} created with IronLauncher`;
+require('./config')(app);
 
 // 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
+const indexRoutes = require('./routes/index.routes');
 // Set up of cart and date inside req.session
 app.use((req, res, next) => {
-  if (!req.session.cart) {
-    req.session.cart = [];
+    if (!req.session.cart) {
+        req.session.cart = [];
+        res.locals.cart = req.session.cart.length;
+    }
     res.locals.cart = req.session.cart.length;
-  }
-  res.locals.cart = req.session.cart.length;
 
-  if (!req.session.date) {
-    req.session.date = { start: null, end: null };
-  }
-  res.locals.date = req.session.date;
-  res.locals.cart = req.session.cart.length;
-  next();
+    if (!req.session.date) {
+        req.session.date = { start: null, end: null };
+    }
+    res.locals.date = req.session.date;
+    res.locals.cart = req.session.cart.length;
+    next();
 });
 
-app.use("/", indexRoutes);
+app.use('/', indexRoutes);
 
-const authRoutes = require("./routes/auth.routes");
-app.use("/auth", authRoutes);
+const authRoutes = require('./routes/auth.routes');
+app.use('/auth', authRoutes);
 
-const adminRoutes = require("./routes/admin.routes");
-app.use("/admin", adminRoutes);
+const adminRoutes = require('./routes/admin.routes');
+app.use('/admin', adminRoutes);
 
 const checkoutRoutes = require('./routes/checkout.routes');
 app.use('/checkout', checkoutRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
--- middlewares/exposeUserInfo.js
+++ middlewares/exposeUserInfo.js
@@ -1,18 +1,12 @@
-// expose to views user information:
-//
-// req.locals.currentUser.username
-// req.locals.currentUser.email
-// req.locals.currentUser.role
-
 const exposeUserInfo = (req, res, next) => {
-  if (req.session.currentUser) {
-    res.locals.currentUser = req.session.currentUser;
-    res.locals.isLoggedIn = true;
-    if (req.session.currentUser.role === "admin") {
-      res.locals.isAdmin = true;
+    if (req.session.currentUser) {
+        res.locals.currentUser = req.session.currentUser;
+        res.locals.isLoggedIn = true;
+        if (req.session.currentUser.role === "admin") {
+            res.locals.isAdmin = true;
+        }
     }
-  }
-  return next();
+    return next();
 };
 
 module.exports = exposeUserInfo;
