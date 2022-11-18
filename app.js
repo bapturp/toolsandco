f6f29eb1b7e81bf351ce67dccfd439559554a5fb
Baptiste Dromer <baptiste.dromer@gmail.com>
2022-11-18 10:03:10 +0100

    refactor index and app

--- app.js
+++ app.js
@@ -1,55 +1,71 @@
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
-require("dotenv").config();
+require('dotenv').config();
 
 // ℹ️ Connects to the database
-require("./db");
+require('./db');
 
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
-const express = require("express");
+const express = require('express');
 
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
-const hbs = require("hbs");
-hbs.registerPartials(__dirname + "/views/partials");
+const hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 const app = express();
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
-require("./config")(app);
+require('./config')(app);
 
-// 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
 // Set up of cart and date inside req.session
 app.use((req, res, next) => {
-  if (!req.session.cart) {
-    req.session.cart = [];
+    if (!req.session.cart) {
+        req.session.cart = [];
+        res.locals.cart = req.session.cart.length;
+    }
     res.locals.cart = req.session.cart.length;
-  }
-  res.locals.cart = req.session.cart.length;
 
-  if (!req.session.date) {
-    req.session.date = { start: null, end: null };
-  }
+    if (!req.session.date) {
+        req.session.date = { start: null, end: null };
+    }
 
-  res.locals.date = req.session.date;
-  res.locals.cart = req.session.cart.length;
-  next();
+    res.locals.date = req.session.date;
+    res.locals.cart = req.session.cart.length;
+    next();
 });
 
-app.use("/", indexRoutes);
+const exposeUserInfo = require('./middlewares/exposeUserInfo');
 
-const authRoutes = require("./routes/auth.routes");
-app.use("/auth", authRoutes);
+const indexRoutes = require('./routes/index.routes');
+app.use('/', indexRoutes);
 
-const adminRoutes = require("./routes/admin.routes");
-app.use("/admin", adminRoutes);
+const authRoutes = require('./routes/auth.routes');
+app.use('/auth', authRoutes);
 
-const checkoutRoutes = require("./routes/checkout.routes");
-app.use("/checkout", checkoutRoutes);
+const adminRoutes = require('./routes/admin.routes');
+app.use('/admin', adminRoutes);
+
+const checkoutRoutes = require('./routes/checkout.routes');
+app.use('/checkout', checkoutRoutes);
+
+const toolsRoutes = require('./routes/tools.routes');
+app.use('/tools', exposeUserInfo, toolsRoutes);
+
+const reservationRoutes = require('./routes/reservation.routes');
+app.use('/reservation', exposeUserInfo, reservationRoutes);
+
+const searchRoutes = require('./routes/search.routes')
+app.use('/search', exposeUserInfo, searchRoutes);
+
+const profileRoutes = require('./routes/profile.routes.js')
+app.use('/profile', exposeUserInfo, profileRoutes);
+
+const cartRoutes = require('./routes/cart.routes')
+app.use('/cart', exposeUserInfo, cartRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
-require("./error-handling")(app);
+require('./error-handling')(app);
 
 module.exports = app;
